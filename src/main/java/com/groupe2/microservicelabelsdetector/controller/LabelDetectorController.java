36d78da272a65ad55e452a69eb07d3685d58c8b6
Adrien Allemand <adrien.allemand@heig-vd.ch>
2023-01-24 14:37:23 +0100

    Feedback

--- src/main/java/com/groupe2/microservicelabelsdetector/controller/LabelDetectorController.java
+++ src/main/java/com/groupe2/microservicelabelsdetector/controller/LabelDetectorController.java
@@ -15,6 +15,10 @@ import java.util.List;
 @RequestMapping("/v1")
 public class LabelDetectorController {
 
+    // TODO ici il y a quelque chose qui ne va pas : cette requête ne modifie pas l'état du microservice donc ça devriat etre un GET
+    // je pense qu'une partie de la confusion viens du fait que cet endpoint a été pensé comme une "fonction" et non comme une "Resource"
+    // qui est sensé être la manière de concevoir les REST API. Je vous invite a consulter le lien dur les REST API Good practice fournis
+    // et/ou venir en discuter avec moi si c'est pas clair.
     @PostMapping("/detect/image")
     public LabelsResponse postDetectImage(@RequestBody UrlToImage image) {
         AwsLabelDetectorHelper awsLabelDetectorHelper = new AwsLabelDetectorHelper();
@@ -22,6 +26,7 @@ public class LabelDetectorController {
         System.out.println("URL : " + image.getUrl() + " maxLabels : " + image.getMaxLabels() + " minConfidence : " + image.getMinConfidence());
         List<LabelObj> labels ;
 
+        // (pas pénalisé) vous pouvez utiliser des paramètres par défault avec @RequestParam voir https://www.baeldung.com/spring-request-param
         if (image.getMaxLabels() != null && image.getMinConfidence() != null){
             labels = awsLabelDetectorHelper.getLabelsFromImage(image.getUrl(), image.getMaxLabels(), image.getMinConfidence());
         }else if(image.getMaxLabels() != null ){
@@ -34,6 +39,7 @@ public class LabelDetectorController {
 
         List<Label> nlabels = labels.stream().map(label -> new Label(label.getName(), label.getConfidence())).toList();
 
+        // (pas pénalisé) mais vous devriez retourner des ResponseEntity et non des objets c'est plus flexible et plus standard
         return new LabelsResponse(nlabels);
     }
 
